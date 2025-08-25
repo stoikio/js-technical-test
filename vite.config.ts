9872bd3507813040ed8282c2358c8173f2f7664d
Martin MARX <martin.marx@stoik.io>
2025-08-25 11:12:39 +0200

    Remove nodemon configuration and update package.json scripts to use tsx for development. Adjust Vite configuration by removing log level setting.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  logLevel: "error",
   server: {
     port: 3000,
     proxy: { "/api": "http://localhost:3001" },
